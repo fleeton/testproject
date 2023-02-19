154125f13963fd788594f74ab7a03467f1def10a
Admin <Admin@DESKTOP-DDDLG3K>
2023-02-20 01:09:41 +0530

    sum

--- testproject/src/testproject/tete.java
+++ testproject/src/testproject/tete.java
@@ -4,6 +4,10 @@ public class tete {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a=4,b=7,sum;
+		sum=a+b;
+		System.out.println("sum is"+sum);
+		
 
 	}
 
